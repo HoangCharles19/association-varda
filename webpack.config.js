9842a30b0f7e4b3d6a297857dba0560f168eab94
HoangCharles19 <hoang.charles1986@gmail.com>
2022-10-21 11:45:22 +0200

    architecture du sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
     main: path.join(__dirname, "src/index.js"),
     form: path.join(__dirname, "src/form/form.js"),
+    topbar: path.join(__dirname, "src/assets/javascripts/topbar.js"),
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -24,18 +27,27 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: "assets/images/[name][ext]",
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       // nom du fichier en sortie en fin de compilation
       filename: "index.html",
       // chemoin du fichier en point d'entrée pour la compilation
       template: path.join(__dirname, "./src/index.html"),
       // chunks = nom du fichier JS en point d'entrée pour webpack
-      chunks: ["main"],
+      chunks: ["main", "topbar"],
     }),
     new HtmlWebpackPlugin({
       filename: "form.html",
       template: path.join(__dirname, "./src/form/form.html"),
-      chunks: ["form"],
+      chunks: ["form", "topbar"],
     }),
   ],
   stats: "minimal",
