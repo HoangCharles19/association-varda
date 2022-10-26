dd59ceb1aba112da4b32c53ade434394164ec987
HoangCharles19 <hoang.charles1986@gmail.com>
2022-10-26 15:33:34 +0200

    création du menu et mise en place du filtre

--- src/index.js
+++ src/index.js
@@ -2,24 +2,36 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const containerNews = document.querySelector(".news-container");
+const containerTopics = document.querySelector(".topic");
 
-const createNews = (actualites) => {
-  const newsNodes = actualites.map((actualite) => {
-    const newsNode = document.createElement("div");
-    newsNode.classList.add("news");
-    newsNode.innerHTML = ` <img
+let actualites;
+let filter;
+
+const createNews = () => {
+  const newsNodes = actualites
+    .filter((actualite) => {
+      if (filter) {
+        return actualite.topic === filter;
+      } else {
+        return true;
+      }
+    })
+    .map((actualite) => {
+      const newsNode = document.createElement("div");
+      newsNode.classList.add("news");
+      newsNode.innerHTML = ` <img
     src="${actualite.photo}"
     alt="profile"
   />
   <h2>${actualite.title}</h2>
   <p class="news-author">${actualite.author} - ${new Date(
-      actualite.createdAt
-    ).toLocaleDateString("fr-FR", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    })}</p>
+        actualite.createdAt
+      ).toLocaleDateString("fr-FR", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })}</p>
   <p class="news-content">
   ${actualite.content}
   </p>
@@ -28,8 +40,8 @@ const createNews = (actualites) => {
     >Supprimer</button>
     <button class="btn btn-primary" data-id="${actualite._id}">Modifier</button>
   </div>`;
-    return newsNode;
-  });
+      return newsNode;
+    });
 
   containerNews.innerHTML = "";
   containerNews.append(...newsNodes);
@@ -63,11 +75,54 @@ const createNews = (actualites) => {
   });
 };
 
+const displayMenuTopics = (topicsArr) => {
+  const liElements = topicsArr.map((topicArr) => {
+    const li = document.createElement("li");
+    li.innerHTML = `  <li>${topicArr[0]} (<strong>${topicArr[1]}</strong>)</li>`;
+    li.addEventListener("click", () => {
+      if (filter === topicArr[0]) {
+        filter = null;
+        li.classList.remove("active");
+      } else {
+        filter = topicArr[0];
+        liElements.forEach((li) => {
+          li.classList.remove("active");
+        });
+        li.classList.add("active");
+      }
+      createNews();
+    });
+    return li;
+  });
+  containerTopics.innerHTML = "";
+  containerTopics.append(...liElements);
+};
+
+const createMenuTopics = () => {
+  const topics = actualites.reduce((acc, actualite) => {
+    if (acc[actualite.topic]) {
+      acc[actualite.topic]++;
+    } else {
+      acc[actualite.topic] = 1;
+    }
+    return acc;
+  }, {});
+
+  const topicsArr = Object.keys(topics)
+    .map((topic) => {
+      return [topic, topics[topic]];
+    })
+    .sort((c1, c2) => c1[0].localeCompare(c2[0]));
+
+  displayMenuTopics(topicsArr);
+};
+
 const fetchArticles = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/news");
-    const actualites = await response.json();
-    createNews(actualites);
+    actualites = await response.json();
+    createNews();
+    createMenuTopics();
   } catch (e) {
     console.error("e:", e);
   }
